19ada0ca83c1edd7c2272fd876a8e36645773f62
genis99 <gvil7492@gmail.com>
2020-08-26 01:29:47 +0200

    wip

--- pages/items.vue
+++ pages/items.vue
@@ -3,8 +3,32 @@
 </template>
 
 <script>
+import moment from 'moment'
+import CryptoJS from 'crypto-js'
+// import axios from 'axios'
+// const md5Hash = CryptoJS.MD5('Test')
+// const sha256Hash = CryptoJS.SHA256('Test1')
+console.log()
+// console.log(sha256Hash.toString())
+window.open('http://api.smitegame.com/smiteapi.svc/createsessionJson/' + this.devId + '/' + this.signature + '/' + this.now, '_blank')
 export default {
-  name: 'Items'
+  name: 'Items',
+  data () {
+    return {
+      message: 'hola',
+      devId: '3465',
+      authKey: 'E8FF28B8096047DBBAD36E981D2AD0D4'
+    }
+  },
+  computed: {
+    now () {
+      return moment().format('YYYYMMDDHHmmss')
+    },
+    signature () {
+      // `this` points to the vm instance
+      return CryptoJS.MD5(this.devId + 'createsession' + this.authKey + this.now)
+    }
+  }
 }
 </script>
 
